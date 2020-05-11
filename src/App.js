cbaec9ea22e28f4b01979c323e990bad22d73308
Shubham <57520514+shubhamaggarwal890@users.noreply.github.com>
2020-05-11 14:59:05 +0530

    Adding facebook data fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  facebookResponse = (response) => {
+    console.log(response);
+
+  }
+  render() {
+    return (
+      <FacebookLogin
+        appId='862008614311891'
+        autoLoad={true}
+        fields='posts'
+        cssClass='my-facebook-button-class'
+        icon='fa-facebook'
+        callback={this.facebookResponse}
+      />
+    )
+  }
 }
 
 export default App;
